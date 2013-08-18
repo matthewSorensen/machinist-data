43b906e1e9573f1d69f224fe4d660b7574fd1500
Matthew Sorensen <matthew.d.sorensen@gmail.com>
2013-08-17 21:29:10 -0700

    totally reformat the imperial data

--- drill.py
+++ drill.py
@@ -1,6 +1,5 @@
 import csv
 
-
 def size_type(name):
     if '/' in name:
         return 'fractional'
@@ -10,23 +9,23 @@ def size_type(name):
         return 'number'
 
 def imperial_drill(filename):
-    raw_sizes = {}
+    sizes = {}
 
     with open(filename, 'rb') as f:
         reader = csv.reader(f, delimiter='\t')
         for line in reader:
-            for i in range(0,min(2, len(line) / 2)): # only take first six rows
-                raw_sizes[line[2*i].strip()] = line[2*i + 1].strip()
+            name = line[0].strip()
+            size = line[1].strip()
+            dtype = size_type(name)
 
-    sizes = {}
-    for name, size in raw_sizes.iteritems():
-        dtype = size_type(name)
-        if dtype == 'number':
-            name = "#" + name
-        sizes[name] = {"nominal" : size, "type" : dtype, "diameter" : 25400 * float(size)}
+            if dtype == 'number':
+                name = "#" + name
+
+            sizes[name] = {"nominal" : size, "type" : dtype, "diameter" : 25400 * float(size)}
+  
     # Add various other sizes - 1" 
     sizes["1"] = {"nominal" : "1.000", "type" : 'fractional', "diameter" : 25400}
         
     return sizes
 
-print imperial_drill('imperial.csv')
+print imperial_drill('imperial_drill.csv')
