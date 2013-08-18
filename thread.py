3706051b0a154fbaf587d8324bfb502703aa5158
Matthew Sorensen <matthew.d.sorensen@gmail.com>
2013-08-17 21:11:28 -0700

    Unify thread record format and add default metric records (ie. M3 as well as M3x0.5)

--- thread.py
+++ thread.py
@@ -1,5 +1,13 @@
+ # -*- coding: utf-8
 import csv
 
+# All screw JSON records have the following attributes:
+# * standard name
+# * pitch (µm)
+# * major diameter
+# * minor diameter
+# Some are optional:
+# * tpi
 
 def imperial_thread(filename):
     raw_threads = {}
@@ -10,11 +18,12 @@ def imperial_thread(filename):
             raw_threads[line[0].strip()+'-'+line[2].strip()] = line
     threads = {}
     for name, cells in raw_threads.iteritems():
+        tpi = int(cells[2].strip())
         threads[name] = {"major" : 25400 * float(cells[1].strip()),
                          "minor" : 25400 * float(cells[3].strip()),
-                         "tpi" : int(cells[2].strip()),
-                         "75%-thread" : 25400 * float(cells[5].strip()),
-                         "50%-thread" : 25400 * float(cells[7].strip())
+                         "tpi" : tpi,
+                         "pitch" : 25400.0 / (1.0 * tpi),
+                         "name" : name,
                          }
     return threads
 
@@ -28,9 +37,18 @@ def metric_thread(filename):
     threads = {}
 
     for diameter, pitches in raw_threads.iteritems():
-        for p in pitches:
-            name = 'M' + diameter + 'x' + p
-            threads[name] = {'major' : 1000 * float(diameter), 'drill' : 1000 * (float(diameter) - float(p)), 'pitch' : 1000 * float(p)}
+        for i,pitch in enumerate(pitches):
+            name = 'M' + diameter + 'x' + pitch
+            udiameter = 1000 * float(diameter)
+            upitch = 1000 * float(pitch)
+
+            record = {'major' : udiameter, 
+                      'minor' : udiameter - 1.082532 * upitch,
+                      'pitch' : upitch,
+                      'name'  : name}
+            threads[name] = record
+            if i == 0: # Also insert the default large thread pitch
+                threads['M' + diameter] = record
 
     return threads
 
